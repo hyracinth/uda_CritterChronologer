56f81a47b98890fe26826cde8b1501dea5f49e62
Jonah Lou <jonahlou@gmail.com>
2020-11-06 20:56:49 -0500

    Added CustomerRepository and implemented save and get customers WIP

---
+++ src/main/java/com/udacity/jdnd/course3/critter/services/CustomerService.java
@@ -0,0 +1,27 @@
+package com.udacity.jdnd.course3.critter.services;
+
+import com.udacity.jdnd.course3.critter.models.Customer;
+import com.udacity.jdnd.course3.critter.models.Pet;
+import com.udacity.jdnd.course3.critter.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CustomerService {
+    @Autowired
+    CustomerRepository customerRepository;
+
+    public List<Customer> getCustomers() {
+        return customerRepository.findAll();
+    }
+
+    public Customer saveCustomer(Customer customer, List<Long> petIds) {
+        List<Pet> pets = new ArrayList<>();
+        // TODO Get list of pets and assign to customer
+
+        return customerRepository.save(customer);
+    }
+}
--- src/main/java/com/udacity/jdnd/course3/critter/user/UserController.java
+++ src/main/java/com/udacity/jdnd/course3/critter/user/UserController.java
@@ -1,8 +1,13 @@
 package com.udacity.jdnd.course3.critter.user;
 
+import com.udacity.jdnd.course3.critter.models.Customer;
+import com.udacity.jdnd.course3.critter.services.CustomerService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.DayOfWeek;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -15,15 +20,22 @@ import java.util.Set;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    @Autowired
+    CustomerService customerService;
 
     @PostMapping("/customer")
     public CustomerDTO saveCustomer(@RequestBody CustomerDTO customerDTO){
-        throw new UnsupportedOperationException();
+        return this.ConvertCustomerToDto(
+                customerService.saveCustomer(this.ConvertDtoToCustomer(customerDTO), null));
     }
 
     @GetMapping("/customer")
     public List<CustomerDTO> getAllCustomers(){
-        throw new UnsupportedOperationException();
+        List<CustomerDTO> listDTO = new ArrayList<>();
+        for(Customer currCustomer : customerService.getCustomers()) {
+            listDTO.add(this.ConvertCustomerToDto(currCustomer));
+        }
+        return listDTO;
     }
 
     @GetMapping("/customer/pet/{petId}")
@@ -49,6 +61,18 @@ public class UserController {
     @GetMapping("/employee/availability")
     public List<EmployeeDTO> findEmployeesForService(@RequestBody EmployeeRequestDTO employeeDTO) {
         throw new UnsupportedOperationException();
+    }
+
+    private static CustomerDTO ConvertCustomerToDto(Customer customerIn) {
+        CustomerDTO customerDTO = new CustomerDTO();
+        BeanUtils.copyProperties(customerIn, customerDTO);
+        return customerDTO;
+    }
+
+    private static Customer ConvertDtoToCustomer(CustomerDTO customerDtoIn) {
+        Customer customer = new Customer();
+        BeanUtils.copyProperties(customerDtoIn, customer);
+        return customer;
     }
 
 }
